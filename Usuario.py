844d21131dbbda68fa605f00e8812187809f402e
MiguelEscalante02 <104610994+MiguelEscalante02@users.noreply.github.com>
2022-05-17 20:36:50 -0500

    arreglo clase escalante

--- Usuario.py
+++ Usuario.py
@@ -2,13 +2,15 @@ class Usuario:
     Nombre:str
     DocumentoUsuario:int
     Contrasena:str
-    def __init__(self,Nombre,DocumentoUsuario,Contrasena):
+    contrasea2:str
+    def __init__(self,Nombre,DocumentoUsuario,Contrasena,contrasea2):
         self.Nombre=Nombre
         self.DocumentoUsuario=DocumentoUsuario
         self.Contrasena=Contrasena
+        self.contrasea2=contrasea2
 
     def printdatos(self):
         print(self.Nombre,self.DocumentoUsuario,self.Contrasena)    
-x=Usuario("tomas","1002544519","123")
+x=Usuario("tomas","1002544519","123","1234")
 x.printdatos()
 
