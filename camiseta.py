b7d07be157cac7c219b12e11ee0e9fcceddea791
sergiocarde <104610830+sergiocarde@users.noreply.github.com>
2022-05-27 11:54:05 -0500

    sergio

--- camiseta.py
+++ camiseta.py
@@ -10,4 +10,18 @@ class camiseta(Producto):
         self.TipoCuello=TipoCuello
         self.Estampada=Estampada
         self.Botones=Botones
+    # al iniciarlo nos pide los datos del main creado en la clase padre y ademas de los datos pedidos en esta clase y guarda la informacion 
+#completa em el archivo de texto
+    def camiseta():
+        outfile = open('Productos.txt','a')
+        TipoCuello= input("Indique el tipo de cuello de la camiseta, redondo o v : ")
+        Estampada= input("Indique si o no, si la camiseta es estampada: ")
+        Botones =input("Indique si o no, si la camseta tiene botones: ")
+        outfile.write("tipo de proucto: camiseta")
+        outfile.write("Cuello: " + TipoCuello+ "\n")
+        outfile.write("cremallera: " + Estampada+"\n" )
+        outfile.write("botones: "+Botones+"\n" )    
+        outfile.write("___________________________________________________________"+"\n")
+
+    camiseta()        
 
