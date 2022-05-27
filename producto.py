6f29188d093ef89cc7235ef841679a48fedab3e9
sergiocarde <104610830+sergiocarde@users.noreply.github.com>
2022-05-27 11:53:10 -0500

    sergio

--- producto.py
+++ producto.py
@@ -13,4 +13,19 @@ class Producto:
         self.Talla=Talla
         self.Genero=Genero
         self.CantidadProducto=CantidadProducto
+    def main():
+        Productos = int(input("Ingrese el número de productos a registrar: "))
+        
+
+        for i in range(Productos):
+            outfile = open('Productos.txt','a')  
+            Referencia= input("Ingrese la referencia: ")
+            NombreProducto= input("Ingrese el nombre del producto: ")
+            ValorProducto=input("ingrese el costo del producto: ")
+            outfile.write("Referencia del producto: " + Referencia+ "\n")
+            outfile.write("Nombre: " + NombreProducto+"\n" )
+            outfile.write("Valor del producto: "+ValorProducto+"\n" )
+            
+            
+    main()    
 
