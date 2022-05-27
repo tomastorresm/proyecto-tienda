259ea1b739bcbefb87b7d10760a2d4963b8630d3
sergiocarde <104610830+sergiocarde@users.noreply.github.com>
2022-05-27 11:57:11 -0500

    Update pantalon.py

--- pantalon.py
+++ pantalon.py
@@ -9,4 +9,13 @@ class pantalon(Producto):
         super().__init__(Referencia, NombreProducto, ValorProducto, Talla, Genero, CantidadProducto)
         self.TipoTela=TipoTela
         self.Rotos=Rotos
-        
+     def pantalon():
+        outfile = open('Productos.txt','a')
+        TipoTela= input("Indique el tipo de tela del pantalon, jean o sudadera: ")
+        Rotos= input("Indique si o no, si el pantalon tiene rotos: ")
+        outfile.write("tipo de proucto: pantalon"+"\n")
+        outfile.write("Cuello: " + TipoTela+ "\n")
+        outfile.write("cremallera: " + Rotos+"\n" )  
+        outfile.write("___________________________________________________________"+"\n")
+
+    pantalon()
