56cb548bf25a69cf360be102e4f1c902d0eaee59
tomastorresm <99606040+tomastorresm@users.noreply.github.com>
2022-05-27 11:56:09 -0500

    Update pantalon.py

--- pantalon.py
+++ pantalon.py
@@ -3,10 +3,10 @@ from producto import Producto
 class pantalon(Producto):
     TipoTela:str
     Rotos:bool
-    Tipo:str
+    
 
-    def __init__(self, Referencia, NombreProducto, ValorProducto, Talla, Genero, CantidadProducto,TipoTela,Rotos,Tipo):
+    def __init__(self, Referencia, NombreProducto, ValorProducto, Talla, Genero, CantidadProducto,TipoTela,Rotos):
         super().__init__(Referencia, NombreProducto, ValorProducto, Talla, Genero, CantidadProducto)
         self.TipoTela=TipoTela
         self.Rotos=Rotos
-        self.Tipo=Tipo
+        
