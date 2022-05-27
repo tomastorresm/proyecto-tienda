de98878bdec537d9076853a9a05fa48fc1c6ae5f
Mateomurillo1 <104611147+Mateomurillo1@users.noreply.github.com>
2022-05-27 16:29:51 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -6,3 +6,17 @@ class Cliente:
         self.Documento=Documento
         self.Celular=Celular
         self.Correo=Correo
+        
+    def cliente():
+        outfile = open('Clientes.txt','a')
+        Nombre=input("Ingrese su nombre: ")
+        Documento= input("Ingrese su documento: ")
+        Celular= input("Ingrese su numero de celular: ")
+        Correo= input("Ingrese su correo electronico: ")
+        outfile.write("Nombre: "+Nombre+"\n")
+        outfile.write("Correo electronico"+Correo+"\n")
+        outfile.write("Documento: " + Documento+ "\n")
+        outfile.write("celular: " + Celular+"\n" )  
+        outfile.write("___________________________________________________________"+"\n")
+
+    cliente()    
