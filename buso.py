77e42df1ef3d17d1881f17df2b3210cb5508a998
sergiocarde <104610830+sergiocarde@users.noreply.github.com>
2022-05-27 11:55:16 -0500

    sergio

--- buso.py
+++ buso.py
@@ -11,5 +11,20 @@ class buso(Producto):
         self.Capota=Capota
         self.Cremallera=Cremallera
         self.Bolsillo=Bolsillo
+        
+    # al iniciarlo nos pide los datos del main creado en la clase padre y ademas de los datos pedidos en esta clase y guarda la informacion 
+#completa em el archivo de texto
+    def busoo():
+        outfile = open('Productos.txt','a')
+        capota= input("Indique si o no, si el buso tien capota: ")
+        cremallera= input("Indique si o no, si el buso tien cremallera: ")
+        bolsillo=input("Indique si o no, si el buso tien bolsillo: ")
+        outfile.write("tipo de producto: buso"+"\n")
+        outfile.write("capota: " + capota+ "\n")
+        outfile.write("cremallera: " + cremallera+"\n" )
+        outfile.write("bolsillo: "+bolsillo+"\n" )  
+        outfile.write("___________________________________________________________"+"\n")
+                  
+    busoo()    
 
     
